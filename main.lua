2cbc39ca5db95a71fc703958b25085597ca40522
Manuel Geissberger <manuel.geissberger@gmail.com>
2025-08-05 23:14:44 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,16 +9,12 @@ function love.load()
 
     -- test grid
     GAMESTATE.engine:getLayerById("main"):drawBorder("█", {0.8, 0.8, 0.8})
-
     local text = "Welcome to the ASCII Roguelike!"
-
     for i = 1, #text + 2 do
         GAMESTATE.engine:getLayerById("main"):setCell(10 + i - 2, 10, "█", {1, 0, 0}, {0, 0, 0})
     end
-
     GAMESTATE.engine:getLayerById("main"):setCell(10, 10, "█", {1, 0, 0}, {0, 0, 0})
     GAMESTATE.engine:getLayerById("layer2"):writeText(10, 10, text, {1, 1, 1})
-    
 end
 
 function love.draw()
